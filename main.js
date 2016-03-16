75871ce005f7c776dee39606f285185b74ef3c66
Charlie Allen <miss.neophyte@gmail.com>
2016-03-16 12:52:58 +0000

    index html completed and starting on main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+var counter = 0, 
+	score = 0;
+
+	var content = [
+		{"name":"Nomad's Dream","answer":"polish"},
+		{"name":"Obsession","answer":"trek"},
+		{"name":"Meet Me On The Star Ferry","answer":"polish"},
+		{"name":"Dangerous Liasion","answer":"polish"},
+		{"name":"What Are Little Girls Made Of?","answer":"trek"},
+		{"name":"In My Back Pocket","answer":"polish"},
+		{"name":"Tomorrow Is Yesterday","answer":"trek"},
+		{"name":"Black Onyx","answer":"polish"},
+		{"name":"Lights of Emerald City","answer":"polish"},
+		{"name":"The Man Trap","answer":"trek"},
+		{"name":"The Naked Time","answer":"trek"},
+		{"name":"What Wizardry Is This?","answer":"polish"},
+		{"name":"This Side Of Paradise","answer":"trek"},
+		{"name":"Eternal Flame","answer":"polish"},
+		{"name":"I Feel The Earth Move","answer":"polish"},
+		{"name":"Day Of The Dove","answer":"trek"},
+		{"name":"The Way To Eden","answer":"trek"},
+		{"name":"Garden of Eden","answer":"polish"},
+		{"name":"Friday's Child","answer":"trek"},
+		{"name":"Days of Wine and Roses","answer":"polish"},
+		{"name":"The Apple","answer":"trek"},
+		{"name":"Thunder Road","answer":"polish"},
+		{"name":"All Our Yesterdays","answer":"trek"},
+		{"name":"Is There No Truth In Beauty?","answer":"trek"}
+	];
